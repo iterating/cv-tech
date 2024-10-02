a7e82d77db1055da72de18132325a89e57fddf49
Jonathan Young <jy>
2024-10-02 11:41:42 -0700

    2024.10.02 after JRE

--- view.mjs
+++ view.mjs
@@ -1,26 +1,26 @@
-export async function fetchYamlResume(url){
+export async function fetchYamlResume(url) {
   try {
-      const response = await fetch(url, {method: "GET"})
-      // console.log(response);
+    const response = await fetch(url, { method: "GET" });
+    // console.log(response);
 
-      const resumeJson = await response.text().then(jsyaml.load); // Convert YAML to JSON
-      
-      // console.log(resumeJson);
-      return(resumeJson);
-   } catch (error) {
-          console.error("Error loading YAML file", error);
-      }
-};
+    const resumeJson = await response.text().then(jsyaml.load); // Convert YAML to JSON
 
-export async function renderResume(data){
-  // Profile section 
+    // console.log(resumeJson);
+    return resumeJson;
+  } catch (error) {
+    console.error("Error loading YAML file", error);
+  }
+}
+
+export async function renderResume(data) {
+  // Profile section
   // document.getElementById('avatar').src = data.sidebar.avatar;
-  document.getElementById('name').innerText = data.sidebar.name;
-  document.getElementById('tagline').innerText = data.sidebar.tagline;
+  document.getElementById("name").innerText = data.sidebar.name;
+  document.getElementById("tagline").innerText = data.sidebar.tagline;
 
   // Education - Populating DOM by iterating over array
-  let educationHTML = '';
-  for (let i=0; i < data.education.info.length; i++){
+  let educationHTML = "";
+  for (let i = 0; i < data.education.info.length; i++) {
     let school = data.education.info[i];
     let detailsMd = marked.parse(school.details);
 
@@ -31,24 +31,23 @@ export async function renderResume(data){
         <div class="timeeducation">${school.time}</div>
         <div class="details">${detailsMd}</div>
       </div>`;
-    }
-  document.getElementById('education-section').innerHTML = educationHTML;
-  
- // Summary
- let summaryHTML = '';
-   let summaryMd = marked.parse(data.careerprofile.summary);
-   console.log(summaryMd)
-   summaryHTML += `
+  }
+  document.getElementById("education-section").innerHTML = educationHTML;
+
+  // Summary
+  let summaryHTML = "";
+  let summaryMd = marked.parse(data.careerprofile.summary);
+  console.log(summaryMd);
+  summaryHTML += `
          <div class="item">
         <div class="details"> ${summaryMd}</div>
         </div>
-     `
- document.getElementById('summary-section').innerHTML = summaryHTML;
+     `;
+  document.getElementById("summary-section").innerHTML = summaryHTML;
 
-  
   // Experience - Populating DOM by using array.forEach
-  let experiencesHTML = '';
-  data.experiences.info.forEach(experience => {
+  let experiencesHTML = "";
+  data.experiences.info.forEach((experience) => {
     let detailsMd = marked.parse(experience.details);
     experiencesHTML += `
       <div class="item">
@@ -62,10 +61,10 @@ export async function renderResume(data){
         <div class="details"> ${detailsMd}</div>
       </div>`;
 
-      // Check for additional roles within the same company
-      if (experience.role2 && experience.time2 && experience.details2) {
-          let detailsMd2 = marked.parse(experience.details2);
-          experiencesHTML += `
+    // Check for additional roles within the same company
+    if (experience.role2 && experience.time2 && experience.details2) {
+      let detailsMd2 = marked.parse(experience.details2);
+      experiencesHTML += `
           <div class="item">
             <div class="meta">
               <div class="upper-row">
@@ -76,22 +75,26 @@ export async function renderResume(data){
             </div>
             <div class="details">${detailsMd2}</div>
           </div>`;
-      }
+    }
   });
-  document.getElementById('experiences').innerHTML = experiencesHTML;
+  document.getElementById("experiences").innerHTML = experiencesHTML;
 
-  // Skills - Populating DOM by using array.map 
-  let skillsHTML = data.skills.toolset.map (skill => `
+  // Skills - Populating DOM by using array.map
+  let skillsHTML = data.skills.toolset
+    .map(
+      (skill) => `
       <div class="item">
-        <h3 class="level-title">${skill.name}</h3>
+        <h6 class="level-title">${skill.name}</h6>
         <div class="level-bar">
           <div class="level-bar-inner" style="width: ${skill.level}%"></div>
         </div>
-      </div>`).join(''); //.join() joins the the html together without separators ∴ no commas
-  document.getElementById('skills-section').innerHTML = skillsHTML;        
-};
+      </div>`
+    )
+    .join(""); //.join() joins the the html together without separators ∴ no commas
+  document.getElementById("skills-section").innerHTML = skillsHTML;
+}
 
-export async function renderPage(){
-    const ready = await fetchYamlResume("./_data/data.yml")
-    renderResume(ready);
-};
+export async function renderPage() {
+  const ready = await fetchYamlResume("./_data/data.yml");
+  renderResume(ready);
+}
